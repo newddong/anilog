5e4dfccec1e79a13cd08446e7b6a463d5cf1a38b
DESKTOP-D8D0OT9\user01 <sw.kwon@pine-partners.com>
2021-10-20 21:38:44 +0900

    미변경

--- src/component/molecules/hashLabel.js
+++ src/component/molecules/hashLabel.js
@@ -8,40 +8,43 @@ import {styles} from '../atom/image/imageStyle';
 export default HashLabel = props => {
 	return (
 		<View style={{flexDirection: 'row', alignItems: 'center'}}>
-			<TouchableOpacity onPress={props.onLabelClick}>
-				<View
-					style={{
-						width: 90 * DP,
-						height: 90 * DP,
-						borderRadius: 100,
-						borderColor: '#FFB6A5',
-						borderWidth: 4,
-						alignItems: 'center',
-						justifyContent: 'center',
-					}}>
-					<View style={{padding: 20 * DP}}>
-						<Hash50 />
-					</View>
+			<View
+				style={{
+					width: 90 * DP,
+					height: 90 * DP,
+					borderRadius: 100,
+					borderColor: '#FFB6A5',
+					borderWidth: 4,
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}>
+				<View style={{padding: 20 * DP}}>
+					{/* 해쉬마크와 바깥원과의 거리 20 */}
+					<Hash50 />
 				</View>
-			</TouchableOpacity>
-			<View style={{marginLeft: 30 * DP, paddingVertical: 2 * DP, }}>
+			</View>
+			<View style={{marginLeft: 30 * DP, paddingVertical: 2 * DP}}>
 				{/* profile Image와 총 height가 4차이가 난다 => paddingVertical 2 */}
-				{props.count == null ? (
+				{/* 해쉬마크가 담긴 원과 KEYWORD간 30의 차이 */}
+				{props.count == null ? ( //부모가 보내는 count값이 없을 경우
+					//키워드만 출력
 					<View>
-						<Text style={[txt.noto30, { width:200*DP} ]} numberOfLines={1} ellipsizeMode="tail">
+						<Text style={[txt.noto30, {width: 200 * DP}]} numberOfLines={1} ellipsizeMode="tail">
 							{props.keyword}
 						</Text>
 					</View>
 				) : (
+					//count값이 있을 경우 'count한 게시물' 출력
 					<View>
 						<View>
-							<Text style={[txt.noto30, {lineHeight: 42 * DP, width:500*DP}]} numberOfLines={1} ellipsizeMode="tail">
+							<Text style={[txt.noto30, {lineHeight: 42 * DP, width: 500 * DP}]} numberOfLines={1} ellipsizeMode="tail">
 								{props.keyword}
 							</Text>
 						</View>
 						<View>
-							<Text
-								style={props.keywordBold ? [txt.noto24b, {lineHeight: 42 * DP,}] : [txt.noto24, {lineHeight: 42 * DP}]}
+							{/* 부모 props의 keywordBold값이 True라면 noto24b를 스타일로 준다 */}
+							<Text 
+								style={props.keywordBold ? [txt.noto24b, {lineHeight: 42 * DP, width: 500 * DP}] : [txt.noto24, {lineHeight: 42 * DP, width: 500 * DP}]}
 								numberOfLines={1}
 								ellipsizeMode="tail">
 								{props.count}
