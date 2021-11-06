45083d895b1022e1e30504973ff6930a6b8a29e6
hjs_desktop <js.hwang@pine-partners.com>
2021-11-07 02:56:38 +0900

    ORGANISM/FeedContent 완료

--- src/component/organism/FeedContent.js
+++ src/component/organism/FeedContent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {organism_style} from './style_organism';
+import {organism_style, feedContent_style} from './style_organism';
 import UserLocationLabel from 'Root/component/molecules/UserLocationLabel';
+import AniButton from 'Root/component/molecules/AniButton';
+import {btn_w130} from 'Root/component/atom/btn/btn_style';
 
 export default FeedContent = props => {
 	//Test용 데이터
-	const data = {
+	const test_data = {
 		user_nickname: 'user_nickname',
 		user_id: 'user_id',
 		user_image: 'https://i.ytimg.com/vi/ERAMkP92arE/maxresdefault.jpg',
@@ -15,12 +17,91 @@ export default FeedContent = props => {
 
 	return (
 		<View style={organism_style.feedContent}>
-			{/* UserLocationLabel */}
+			{/* line 1 */}
 			<View style={[organism_style.userLocationLabel_view_feedContent]}>
+				{/* UserLocationLabel */}
 				<View style={[organism_style.userLocationLabel_feedContent]}>
-					<UserLocationLabel data={data} onLabelClick={e => alert(e)} />
+					<UserLocationLabel data={test_data} onLabelClick={e => alert(e)} />
 				</View>
+
+				{/* type값이 status일 경우 status 버튼이 나오고 그렇지 않으면 다른 버튼 표기 */}
+				{props.data.type == 'status' ? (
+					<>
+						<View style={[feedContent_style.status]}>
+							<AniButton
+								btnLayout={[btn_w130]}
+								btnTitle={'Status'}
+								btnTheme={'noShadow'}
+								btnStyle={'border'}
+								titleFontStyle={24}
+								onPress={() => alert('Onpress')}
+							/>
+						</View>
+
+						<View style={[organism_style.meatball, feedContent_style.meatball]}>
+							<Text>MEATBALL 50 HORIZONTAL</Text>
+						</View>
+					</>
+				) : (
+					<View style={[organism_style.button_view_feedContent]}>
+						<View style={[organism_style.favoriteTag_view_feedContent]}>
+							<View style={[organism_style.favoriteTag_feedContent]}>
+								<Text>FavoriteTag48</Text>
+							</View>
+							<View style={[organism_style.like_count_feedContent, feedContent_style.like_count]}>
+								<Text>like_count</Text>
+							</View>
+						</View>
+						<View style={[organism_style.share48_view_feedContent]}>
+							<View style={[organism_style.share48_feedContent]}>
+								<Text>Share48</Text>
+							</View>
+							<View style={[organism_style.share_feedContent, feedContent_style.share]}>
+								<Text>공유</Text>
+							</View>
+						</View>
+					</View>
+				)}
+			</View>
+
+			{/* line 1-1 (제보관련 내용) */}
+			{props.data.tipOff && (
+				<View style={[organism_style.tipOff_feedContent, feedContent_style.tipOff]}>
+					<Text>제보 날짜: 20201.10.22\n제보장소: 경기도 김포시 김포한강8로 16-6 인근 비닐 하우스</Text>
+				</View>
+			)}
+
+			{/* line 2 */}
+			<View style={[organism_style.content_feedContent, feedContent_style.content]}>
+				<Text>Content</Text>
+			</View>
+
+			{/* line 3 */}
+			<View style={[organism_style.time_view_feedContent]}>
+				<View style={[organism_style.time_feedContent]}>
+					<Text>time시간 전</Text>
+				</View>
+
+				{/* 내용이 길어지면 더보기 버튼이 생기는 로직은 추후 구현 */}
+				{props.data.addMore && (
+					<View style={[organism_style.addMore_view_feedContent]}>
+						<View style={[organism_style.addMore_feedContent]}>
+							<Text>더보기</Text>
+						</View>
+						<View style={[organism_style.braket]}>
+							<Text>BRACKET 48</Text>
+						</View>
+					</View>
+				)}
 			</View>
 		</View>
 	);
+};
+
+FeedContent.defaultProps = {
+	data: {
+		type: 'button',
+		addMore: true,
+		tipOff: true,
+	},
 };
--- src/component/organism/style_organism.js
+++ src/component/organism/style_organism.js
@@ -3,17 +3,19 @@ import DP from 'Root/config/dp';
 
 export const organism_style = StyleSheet.create({
 	feedContent: {
+		flexDirection: 'column',
 		width: 750 * DP,
 		alignItems: 'center',
 		justifyContent: 'center',
 		backgroundColor: '#EDE8A6',
 	},
 	userLocationLabel_view_feedContent: {
+		flexDirection: 'row',
 		width: 654 * DP,
 		marginTop: 78 * DP,
-		backgroundColor: '#0F0000',
 	},
 	userLocationLabel_feedContent: {
+		flexDirection: 'row',
 		width: 448 * DP,
 		backgroundColor: '#0FA743',
 	},
@@ -50,5 +52,129 @@ export const organism_style = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 		backgroundColor: '#C1DAF6',
+	},
+	meatball: {
+		width: 50 * DP,
+		height: 50 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#CAD0F2',
+	},
+	content_feedContent: {
+		width: 654 * DP,
+		height: 86 * DP,
+		justifyContent: 'center',
+		backgroundColor: '#0FA743',
+	},
+	time_view_feedContent: {
+		flexDirection: 'row',
+		width: 654 * DP,
+		height: 48 * DP,
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		backgroundColor: '#C1EDD0',
+	},
+	time_feedContent: {
+		flexDirection: 'row',
+		width: 110 * DP,
+		height: 36 * DP,
+		justifyContent: 'center',
+		backgroundColor: '#F2CACA',
+	},
+	addMore_view_feedContent: {
+		flexDirection: 'row',
+		width: 108 * DP,
+		height: 48 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	addMore_feedContent: {
+		width: 60 * DP,
+		height: 36 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#F2CACA',
+	},
+	braket: {
+		width: 48 * DP,
+		height: 48 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#CAD0F2',
+	},
+	tipOff_feedContent: {
+		width: 654 * DP,
+		justifyContent: 'center',
+		backgroundColor: '#0FA743',
+	},
+	button_view_feedContent: {
+		flexDirection: 'row',
+		width: 126 * DP,
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		backgroundColor: '#C1EDD0',
+	},
+	favoriteTag_view_feedContent: {
+		flexDirection: 'column',
+		width: 48 * DP,
+		height: 84 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#4B6EE1',
+	},
+	favoriteTag_feedContent: {
+		width: 48 * DP,
+		height: 48 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#AEB7EF',
+	},
+	like_count_feedContent: {
+		width: 44 * DP,
+		height: 35 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#EED3D3',
+	},
+	share48_view_feedContent: {
+		width: 48 * DP,
+		height: 84 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	share48_feedContent: {
+		width: 48 * DP,
+		height: 48 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#AEB7EF',
+	},
+	share_feedContent: {
+		width: 44 * DP,
+		height: 35 * DP,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#4B6EE1',
+	},
+});
+
+export const feedContent_style = StyleSheet.create({
+	status: {
+		marginLeft: 14 * DP,
+	},
+	meatball: {
+		marginLeft: 12 * DP,
+	},
+	content: {
+		marginTop: 20 * DP,
+	},
+	tipOff: {
+		marginTop: 16 * DP,
+	},
+	like_count: {
+		marginTop: 1 * DP,
+	},
+	share: {
+		marginTop: 1 * DP,
 	},
 });
