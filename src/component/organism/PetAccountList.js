c91a0ce600ff9af8bd81cfe368b5aad450df005f
hjs_desktop <js.hwang@pine-partners.com>
2021-11-08 00:48:42 +0900

    PetAccountList 파일 삭제

--- src/component/organism/PetAccountList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {organism_style, petAccountList_style} from './style_organism';
-import FeedContent from './FeedContent';
-import {FlatList} from 'react-native';
-
-export default PetAccountList = props => {
-	return <View style={organism_style.PetAccountList_main}></View>;
-};
