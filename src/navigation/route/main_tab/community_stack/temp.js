8b5418d3370976c0926a9ca087f2295a024c2851
lky <newddong@hanmail.net>
2021-11-08 19:28:50 +0900

    네비게이션 작업 완료

---
+++ src/navigation/route/main_tab/community_stack/temp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+export default Temp = () => {
+
+
+    return (
+        <View>
+            <Text>커뮤니티는 이후 버전에 출시될 예정입니다.</Text>
+        </View>
+    )
+
+}
---
+++ src/navigation/route/main_tab/protection_stack/ProtectionStackNavigation.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { SafeAreaView, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, } from '@react-navigation/stack';
+import Profile from "Templete/Profile";
+import SocialRelation from "Templete/SocialRelation";
+import FeedList from "Templete/FeedList";
+// import AnimalProtectRequestDetail from "Templete/AnimalProtectRequestDetail";
+import FeedListForHashTag from "Templete/FeedListForHashTag";
+import FeedCommentList from "Templete/FeedCommentList";
+import MissingAnimalDetail from "Templete/MissingAnimalDetail";
+import ReportDetail from "Templete/ReportDetail";
+import ActivationDetail from "Templete/ActivationDetail";
+
+import ProtectionTopTabNavigation from "./protection_tab/ProtectionTopTabNavigation";
+
+import { PIC_SELECTION, } from 'Root/i18n/msg';
+
+const ProtectionStack = createStackNavigator();
+
+export default ProtectionStackNavigation = () => {
+    return (
+        <ProtectionStack.Navigator initialRouteName="Login">
+            <ProtectionStack.Screen name="ProtectionTab" component={ProtectionTopTabNavigation} />
+
+            <ProtectionStack.Screen name="UserProfile" component={Profile} />
+            <ProtectionStack.Screen name="SocialRelation" component={SocialRelation} />
+            <ProtectionStack.Screen name="UserFeedList" component={FeedList} />
+            <ProtectionStack.Screen name="HashFeedList" component={FeedList} />
+            <ProtectionStack.Screen name="ProtectAnimalFeedList" component={FeedList} />
+            <ProtectionStack.Screen name="UserTagFeedList" component={FeedList} />
+            {/* <ProtectionStack.Screen name="AnimalProtectRequestDetail" component={AnimalProtectRequestDetail} /> */}
+            <ProtectionStack.Screen name="FeedListForHashTag" component={FeedListForHashTag} />
+            <ProtectionStack.Screen name="FeedCommentList" component={FeedCommentList} />
+            
+            {/* <ProtectionStack.Screen name="AnimalProtectRequestDetail" component={AnimalProtectRequestDetail} /> */}
+            <ProtectionStack.Screen name="MissingAnimalDetail" component={MissingAnimalDetail} />
+            <ProtectionStack.Screen name="ReportDetail" component={ReportDetail} />
+            <ProtectionStack.Screen name="ActivationDetail" component={ActivationDetail} />
+        </ProtectionStack.Navigator>
+    );
+};
