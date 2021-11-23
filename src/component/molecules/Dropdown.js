0f8f97b16232dcf1c5bb82ed4a27f3b7f019f47a
DESKTOP-TINVNIK\lky <newddong@hanmail.net>
2021-11-24 02:29:23 +0900

    [기영]메인탭, 미트볼 드롭다운 작업

--- src/component/molecules/Dropdown.js
+++ src/component/molecules/Dropdown.js
@@ -9,6 +9,7 @@ import AniButton from './AniButton';
  * @param {{
  * buttonComponent : '',
  * dropdownList : '',
+ * horizontalOffset : '',
  * }} props
  */
 export default Dropdown = props => {
@@ -36,7 +37,7 @@ export default Dropdown = props => {
 	const closeDropdown = () => {
 		console.log('closeDropDown');
 		Modal.close();
-		buttonref.current.press();
+		buttonref.current&&buttonref.current.press();
 		props.buttonComponent.props.onClose();
 	}
 	const positionY = (py,height) => {
@@ -47,13 +48,12 @@ export default Dropdown = props => {
 		}
 	};
 
-	const positionX = (px, width) => {
-		if (props.alignRight) {
-			return px - width;
-		} else {
+	const positionX = (px,width) => {
+		if(props.horizontalOffset){
+			return px-props.horizontalOffset+width;
+		}else{
 			return px;
 		}
-
 	}
 
 	const click = () => {
@@ -93,5 +93,5 @@ Dropdown.defaultProps = {
 	dropdownList: <View style={{ position: 'absolute', width: 100, height: 100, backgroundColor: 'blue' }} />,
 	buttonComponent: <View style={{ position: 'absolute', width: 100, height: 100, backgroundColor: 'white' }} />,
 	alignBottom: false,
-	alignRight: false,
+	horizontalOffset : 0,
 };
--- src/navigation/maintab/style_BottomTab.js
+++ src/navigation/maintab/style_BottomTab.js
@@ -8,43 +8,20 @@ export const tab = StyleSheet.create({
 		height: 140 * DP,
 	},
 	hitboxLayout: {
-		width: 100 * DP,
-		height: 80 * DP,
+		width: 150 * DP,
+		height: 140 * DP,
 		alignItems: 'center',
 	},
 	tab_feed: {
-		marginTop: 24 * DP,
-		marginBottom: 4 * DP,
-	},
-	tab_video: {
-		marginTop: 28 * DP,
-		marginBottom: 8 * DP,
+		paddingTop: 24 * DP,
 	},
 	tab_animal_saving: {
-		marginTop: 26 * DP,
-		marginBottom: 6 * DP,
+		paddingTop: 26 * DP,
+	},
+	tab_community: {
+		paddingTop: 24 * DP,
 	},
 	tab_my: {
-		marginTop: 28 * DP,
-		marginBottom: 8 * DP,
-	},
-});
-
-export const layout = StyleSheet.create({
-	tab_feed: {
-		width: 42 * DP,
-		height: 50 * DP,
-	},
-	tab_video: {
-		width: 54 * DP,
-		height: 42 * DP,
-	},
-	tab_animal_saving: {
-		width: 58 * DP,
-		height: 46 * DP,
-	},
-	tab_my: {
-		width: 46 * DP,
-		height: 42 * DP,
+		paddingTop: 24 * DP,
 	},
 });
