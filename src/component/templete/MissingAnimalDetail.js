da61f07fe12fed280d34aefa35a0eff41c470ef2
hjs_m1 <js.hwang@pine-partners.com>
2021-11-23 20:41:06 +0900

    [준상] - 실종 게시물 insert API 작업 완료. select 부분 작업 필요

--- src/component/templete/MissingAnimalDetail.js
+++ src/component/templete/MissingAnimalDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, ScrollView, LogBox } from 'react-native';
-import { login_style, missingAnimalDetail, temp_style } from './style_templete';
-import { SvgWrap } from 'Screens/svgwrapper';
-import { Post_dog } from 'Asset/image';
+import {View, ScrollView, LogBox} from 'react-native';
+import {login_style, missingAnimalDetail, temp_style} from './style_templete';
+import {SvgWrap} from 'Screens/svgwrapper';
+import {Post_dog} from 'Asset/image';
 import FeedContent from '../organism/FeedContent';
 import CommentList from '../organism_ksw/CommentList';
-import { useNavigation } from '@react-navigation/core';
+import {useNavigation} from '@react-navigation/core';
 
 export default MissingAnimalDetail = props => {
 	const navigation = useNavigation();
@@ -14,62 +14,73 @@ export default MissingAnimalDetail = props => {
 		LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
 	}, []);
 
-	const [photo, setPhoto] = React.useState(props.route.params != null ? props.route.params : []) //PhotoSelect에서 사진 선택이 됐을 경우 photo에 담김
+	const [photo, setPhoto] = React.useState(props.route.params != null ? props.route.params : []); //PhotoSelect에서 사진 선택이 됐을 경우 photo에 담김
 	const [editComment, setEditComment] = React.useState(false); //답글 쓰기 클릭 state
 	const [privateComment, setPrivateComment] = React.useState(false); // 공개 설정 클릭 state
-	const [replyText, setReplyText] = React.useState()
+	const [replyText, setReplyText] = React.useState();
 
 	React.useEffect(() => {
-		setPhoto(props.route.params)
-	}, [props.route.params])
+		setPhoto(props.route.params);
+	}, [props.route.params]);
 
 	React.useEffect(() => {
 		navigation.addListener('blur', () => {
-			setPhoto([])
-		})
-	})
+			setPhoto([]);
+		});
+	});
 
+	const [data, setData] = React.useState({
+		comment_photo_uri: '', //댓글 첨부 이미지 uri
+		comment_contents: '', //댓글 내용
+		comment_is_secure: '', //true일때는 writer와 댓글이 달린 게시글 작성자만 볼수있음,
+		comment_feed_id: '', //댓글이 작성된 피드 게시물
+	});
 
 	//답글 쓰기 => Input 작성 후 보내기 클릭 콜백 함수
 	const onWrite = () => {
 		console.log('onWrite', replyText);
+		console.log('commentData=>' + commentData.comment_contentsdsf);
+		setCommentData({...commentData, comment_contents: replyText, comment_is_secure: privateComment, comment_feed_id: ''});
 	};
 
 	// 답글 쓰기 -> 자물쇠버튼 클릭 콜백함수
 	const onLockBtnClick = () => {
 		setPrivateComment(!privateComment);
-		!privateComment ? alert('비밀댓글로 설정되었습니다.') : alert('댓글이 공개설정되었습니다.')
+		!privateComment ? alert('비밀댓글로 설정되었습니다.') : alert('댓글이 공개설정되었습니다.');
 	};
 
 	// 답글 쓰기 -> 이미지버튼 클릭 콜백함수
 	const onAddPhoto = () => {
-		navigation.push('SinglePhotoSelect', props.route.name)
+		navigation.push('SinglePhotoSelect', props.route.name);
 	};
 
 	// 답글 쓰기 -> Input value 변경 콜백함수
 	const onChangeReplyInput = text => {
-		setReplyText(text)
+		console.log('onChangeReplyInput');
+		setReplyText(text);
 	};
 
 	// 답글 쓰기 버튼 클릭 콜백함수
 	const onReplyBtnClick = () => {
-		setEditComment(!editComment)
+		console.log('onReplyBtnClick');
+		setEditComment(!editComment);
 	};
 
 	// 자식 답글에서 답글쓰기 버튼 클릭 콜백함수
 	const onChildReplyBtnClick = comment => {
-		setEditComment(!editComment)
+		console.log('onChildReplyBtnClick');
+		setEditComment(!editComment);
 	};
 
 	// 답글 이미지 등록 후 지우기 버튼 클릭 콜백함수
 	const onDeleteImage = () => {
-		setPhoto([])
-	}
+		console.log('onDeleteImage');
+		setPhoto([]);
+	};
 
 	return (
 		<View style={[login_style.wrp_main]}>
 			<ScrollView contentContainerStyle={missingAnimalDetail.insideContainer}>
-
 				{/* Poster */}
 				<View style={[missingAnimalDetail.poster]}>
 					<SvgWrap svg={<Post_dog />} />
@@ -82,7 +93,6 @@ export default MissingAnimalDetail = props => {
 					<CommentList onPressReplyBtn={onReplyBtnClick} onPress_ChildComment_ReplyBtn={comment => onChildReplyBtnClick(comment)} />
 				</View>
 				{/* Parent Comment 혹은 Child Comment 에서 답글쓰기를 클릭할 시 화면 최하단에 등장 */}
-
 			</ScrollView>
 
 			{editComment ? (
