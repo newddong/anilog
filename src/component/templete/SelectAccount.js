4fa21581fc5e649cac8e3dd26bcf64dc6db692df
hjs_desktop <js.hwang@pine-partners.com>
2021-12-05 21:34:57 +0900

    [준상] - 반려동물 정보수정 팝업창 연결

--- src/component/templete/SelectAccount.js
+++ src/component/templete/SelectAccount.js
@@ -4,11 +4,21 @@ import AccountList from '../organism_ksw/AccountList';
 import {login_style, selectAccount} from './style_templete';
 
 export default SelectAccount = props => {
+	const onSelect = (item, index) => {
+		Modal.popTwoBtn(
+			`${index}님이 입양예정자가 맞습니까?`,
+			'취소',
+			'예',
+			() => Modal.close(),
+			() => alert('확인!'),
+		);
+	};
+
 	return (
 		<View style={[login_style.wrp_main, selectAccount.container]}>
 			{/* AccountList */}
 			<View style={[selectAccount.accountList]}>
-				<AccountList />
+				<AccountList onSelect={(item, index) => onSelect(item, index)} />
 			</View>
 		</View>
 	);
