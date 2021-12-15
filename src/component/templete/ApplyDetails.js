0bfe1fd18603766bf01605abafaeff06bc1bbd58
hjs_desktop <js.hwang@pine-partners.com>
2021-12-15 10:55:07 +0900

    [준상] - 입양 신청 내역, 임시보호 신청 내역 필드 상세 지정(view).

--- src/component/templete/ApplyDetails.js
+++ src/component/templete/ApplyDetails.js
@@ -10,6 +10,7 @@ import AniButton from '../molecules/AniButton';
 import AnimalProtectDetail from '../organism_ksw/AnimalProtectDetail';
 import {applyDetails, btn_style, login_style, temp_style} from './style_templete';
 import {CommonActions} from '@react-navigation/native';
+import {getApplyAdoptionTempProtectDetail} from 'Root/api/appliesapi_hjs';
 
 export default ApplyDetails = ({route, navigation}) => {
 	const [data, setData] = React.useState({...route.params});
@@ -23,6 +24,59 @@ export default ApplyDetails = ({route, navigation}) => {
 		// console.log('data details', data);
 		// Modal.close()
 	}, [data]);
+
+	//입양신청 및 임시보호 신청 내역 상세 데이터 불러오기
+	// React.useEffect(() => {
+	// 	console.log('- ApplyDetails - ');
+	// 	getApplyAdoptionTempProtectDetail(
+	// 		{
+	// 			userobject_id: props.route.params.userobject_id,
+	// 			protect_act_type: props.route.params.protect_act_type,
+	// 		},
+	// 		data => {
+	// 			console.log('data' + JSON.stringify(`data${data}`));
+	// 			setData(data);
+	// 		},
+	// 	);
+	// }, [props.route.params]);
+
+	// //ProtectionActivityApplicantObject
+	// city : '',  //시,도,군
+	// district  : '', //구
+	// neighbor  : '', //읍,면,동
+	// brief  : '', //보호장소
+	// detail   : '', //보호장소
+	// protect_act_phone_number : String,  //보호 신청자의 전화번호
+
+	// protect_act_companion_history : Array<{
+	// 	companion_pet_species : String,
+	// 	companion_pet_age : String,
+	// 	companion_pet_period : String,
+	// 	companion_pet_current_status : Enum('living','died','adopted') //상태정보 카테고리 정해야함
+	// 	}>, //보호 신청자의 반려생활 이력
+
+	// protect_act_checklist : {
+	// 	is_adult : Boolean, //성인여부
+	// 	is_near_veterinary : Boolean, //보호지 근처의 동물병원 여부
+	// 	is_agreed_housemate : Boolean, //가족, 동거인의 동의 여부
+	// 	is_experience_defecate : Boolean, //배변훈련 지식여부
+	// 	is_knowledge_sanitation : Boolean, //반려동물 미용,위생 지식여부
+	// 	}, //보호신청 체크리스트
+
+	// protect_act_motivation //보호활동 신청 동기
+
+	// //ProtectRequestObject
+	// protect_request_photo_thumbnail: '',//보호요청 게시물 썸네일 uri
+	// protect_request_status : '', //상태 [입양가능(rescue),협의중(discuss),안락사 임박(nearrainbow), 완료(complete), 사망(rainbowbridge)]
+	// protect_request_date //보호요청 게시글 작성일시
+
+	//ShelterProtectAnimalObject
+	// protect_animal_rescue_location : '', //보호중인 동물의 구조장소
+	// protect_animal_species : '', //보호중인 동물의 종류(ex 개, 고양이, 토끼)
+	// protect_animal_species_detail : '', //보호중인 동물의 종류(ex 리트리버, 푸들, 진돗개)
+	// protect_animal_sex : '', //보호중인 동물의 성별
+
+	// bookmark: false,
 
 	//모달창에서 최종 확인을 클릭
 	const onFinalize = () => {
