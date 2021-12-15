ce0669b1784a90b3fc35fea3fbfaf9dfb7d689e7
hjs_desktop <js.hwang@pine-partners.com>
2021-12-15 10:22:37 +0900

    [준상] - 입양신청 및 임시보호 신청 리스트 함수명 수정, 신청내역 상세정보 API 초안 작업 완료

--- src/api/appliesapi_hjs.js
+++ src/api/appliesapi_hjs.js
@@ -3,7 +3,7 @@ import {serveruri, cookieReset} from 'Screens/server';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 /**
- * 신청내역 가져오기 (입양신청, 임시보호 신청, 봉사활동 신청)
+ * 신청내역 가져오기 (입양신청, 임시보호 신청, 봉사활동 신청 - 한 화면에서 입양신청 1개, 임시보호 신청 1개, 봉사활동 신청 3 ~ 4개 표출)
  *
  * @param {string} params.userobject_id - 사용자 계정 id
  */
@@ -25,25 +25,49 @@ export const getAppliesRecord = async (params, callback) => {
 };
 
 /**
- * 신청내역 가져오기 (입양신청, 임시보호 신청, 봉사활동 신청)
+ * 입양신청 및 임시보호 신청 리스트 데이터 불러오기 (입양신청, 임시보호 신청을 하나의 함수로 사용)
  *
  * @param {string} params.userobject_id - 사용자 계정 id
- * @param {string} params.protect_act_type - 사용자 계정 id
+ * @param {string} params.protect_act_type - 신청 타입
  */
-export const getApplyAdoptionList = async (params, callback) => {
+export const getApplyAdoptionTempProtectList = async (params, callback) => {
 	try {
-		let recieved = await axios.post(serveruri + '/post/getAppliesRecord', {
+		let recieved = await axios.post(serveruri + '/post/getApplyAdoptionTempProtectList', {
 			userobject_id: params.userobject_id,
 			protect_act_type: params.protect_act_type,
 		});
 		const data = recieved.data;
-		console.log(`getAppliesRecord data:${data}`);
+		console.log(`getApplyAdoptionTempProtectList data:${data}`);
 		if (data.status === 200) {
 			callback(data);
 		} else {
-			console.log('getAppliesRecord Network Error : ' + JSON.stringify(msg));
+			console.log('getApplyAdoptionTempProtectList Network Error : ' + JSON.stringify(msg));
 		}
 	} catch (err) {
-		console.log('getAppliesRecord Code Error : ' + JSON.stringify(err));
+		console.log('getApplyAdoptionTempProtectList Code Error : ' + JSON.stringify(err));
+	}
+};
+
+/**
+ * 신청내역 상세정보 가져오기 (입양신청, 임시보호 신청을 하나의 함수로 사용)
+ *
+ * @param {string} params.userobject_id - 사용자 계정 id
+ * @param {string} params.protect_act_type - 신청 타입
+ */
+export const getApplyAdoptionTempProtectDetail = async (params, callback) => {
+	try {
+		let recieved = await axios.post(serveruri + '/post/getApplyAdoptionTempProtectDetail', {
+			userobject_id: params.userobject_id,
+			protect_act_type: params.protect_act_type,
+		});
+		const data = recieved.data;
+		console.log(`getApplyAdoptionTempProtectDetail data:${data}`);
+		if (data.status === 200) {
+			callback(data);
+		} else {
+			console.log('getApplyAdoptionTempProtectDetail Network Error : ' + JSON.stringify(msg));
+		}
+	} catch (err) {
+		console.log('getApplyAdoptionTempProtectDetail Code Error : ' + JSON.stringify(err));
 	}
 };
--- src/component/templete/ApplyAdoptionList.js
+++ src/component/templete/ApplyAdoptionList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import AnimalNeedHelpList from '../organism_ksw/AnimalNeedHelpList';
 import {login_style, temp_style, baseInfo_style} from './style_templete';
+import {getApplyAdoptionTempProtectList} from 'Root/api/appliesapi_hjs';
 
 // 각각 뷰에 컴포넌트 삽입시 style의 첫번째 index 삭제할 것. 두번째 index는 상.하 간격 style이라서 이 컴포넌트에만 해당 됨.
 //ex) 변경 전: <View style={[btn_style.btn_w654, findAccount_style.btn_w654]}>   변경 후:  <View style={[findAccount_style.btn_w654]}>
@@ -17,10 +18,10 @@ export default ApplyAdoptionList = props => {
 		navigation.push('ApplyAdoptionDetails', item);
 	};
 
-	//입양신청 및 임시보호 신청 데이터 불러오기
+	//입양신청 및 임시보호 신청 리스트 데이터 불러오기
 	// React.useEffect(() => {
 	// 	console.log('- ApplyAdoptionList - ');
-	// 	getApplyAdoptionList(
+	// 	getApplyAdoptionTempProtectList(
 	// 		{
 	// 			userobject_id: props.route.params.userobject_id,
 	// 			protect_act_type: props.route.params.protect_act_type,
