590cff6edc367ee7e212c5050b0da0e4bb2f9375
이재현 <jhlsuper@gmail.com>
2022-01-05 11:29:53 +0900

    [재현] 반려동물 몸무게 입력 ios 키보드 타입 변경

--- src/component/templete/SetPetInformation.js
+++ src/component/templete/SetPetInformation.js
@@ -156,7 +156,7 @@ export default SetPetInformation = ({route, navigation}) => {
 							onChange={onChangeKg}
 							value={data.pet_weight}
 							validator={weigthValid}
-							keyboardType={'number-pad'}
+							keyboardType={'numeric'}
 							maxLength={4}
 							confirm_msg=""
 						/>
